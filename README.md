fb31c1ff3565c587d4408439ff638f079f1b26dc
mcjmigdal <mcjmigdal@gmail.com>
2023-08-07 15:22:06 +0200

    create repository

---
+++ README.md
@@ -0,0 +1,19 @@
+# scrapeORCID
+
+Get publication list from ORCID.
+
+## USAGE
+
+`orcid2cite.py ORCIDID [prefix='<li>' suffix='</li>']`
+
+## Example
+
+```
+> ./orcid2cite.py 0000-0002-8021-7263
+<li>Uszczynska-Ratajczak, B., Sugunan, S., Kwiatkowska, M., Migdal, M., Carbonell-Sala, S., Sokol, A., Winata, C.L., and Chacinska, A. 2022. Profiling subcellular localization of nuclear-encoded mitochondrial gene products in zebrafish. Life Sci. Alliance. 6:e202201514. doi:https://doi.org/10.26508/lsa.202201514.</li>
+<li>Migdał, M., Arakawa, T., Takizawa, S., Furuno, M., Suzuki, H., Arner, E., Winata, C.L., and Kaczkowski, B. 2023. xcore: an R package for inference of gene expression regulators. BMC Bioinformatics. 24:14. doi:https://doi.org/10.1186/s12859-022-05084-0.</li>
+<li>Migdał, M., Tralle, E., Abu Nahia, K., Bugajski, Ł., Kędzierska, K.Z., Garbicz, F., Piwocka, K., Winata, C.L., and Pawlak, M. 2021. Multi-omics analyses of early liver injury reveals cell-type-specific transcriptional and epigenomic shift. BMC Genomics. 22:904. doi:https://doi.org/10.1186/s12864-021-08173-1.</li>
+<li>Abu Nahia, K., Migdał, M., Quinn, T.A., Poon, K., Łapiński, M., Sulej, A., Liu, J., Mondal, S.S., Pawlak, M., Bugajski, Ł., Piwocka, K., Brand, T., Kohl, P., Korzh, V., and Winata, C. 2021. Genomic and physiological analyses of the zebrafish atrioventricular canal reveal molecular building blocks of the secondary pacemaker region. Cell. Mol. Life Sci.. 78:6669-6687. doi:https://doi.org/10.1007/s00018-021-03939-y.</li>
+<li>Migdal, M., Ruan, D.F., Forrest, W.F., Horowitz, A., and Hammer, C. 2021. MiDAS—Meaningful Immunogenetic Data at Scale. PLoS Comput Biol. 17:e1009131. doi:https://doi.org/10.1371/journal.pcbi.1009131.</li>
+<li>Pawlak, M., Kedzierska, K.Z., Migdal, M., Nahia, K.A., Ramilowski, J.A., Bugajski, L., Hashimoto, K., Marconi, A., Piwocka, K., Carninci, P., and Winata, C.L. 2019. Dynamics of cardiomyocyte transcriptome and chromatin landscape demarcates key events of heart development. Genome Res.. 29:506-519. doi:https://doi.org/10.1101/gr.244491.118.</li>
+```
---
+++ orcid2cite.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# By Migdał
+# get citations for given ORCID ID
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from sys import argv, exit
+import re, requests
+
+usage = "orcid2cite.py ORCIDID [prefix='<li>' suffix='</li>']"
+
+
+class CrossRefClient(object):
+    """source https://gist.github.com/dobrosketchkun/f14e1ab9ae817b00b28251f11786fbcf"""
+
+    def __init__(self, accept='text/x-bibliography; style=apa', timeout=3):
+        """
+        # Defaults to APA biblio style
+
+        # Usage:
+        s = CrossRefClient()
+        print s.doi2apa("10.1038/nature10414")
+        """
+        self.headers = {'accept': accept}
+        self.timeout = timeout
+
+    def query(self, doi, q={}):
+        if doi.startswith("http://"):
+            url = doi
+        else:
+            url = "http://dx.doi.org/" + doi
+        r = requests.get(url, headers = self.headers)
+        return r
+
+    def doi2apa(self, doi):
+        self.headers['accept'] = 'text/x-bibliography; style=apa'
+        return self.query(doi).text
+
+    def doi2turtle(self, doi):
+        self.headers['accept'] = 'text/turtle'
+        return self.query(doi).text
+
+    def doi2json(self, doi):
+        self.headers['accept'] = 'application/vnd.citationstyles.csl+json'
+        return self.query(doi).json()
+
+
+def names(ref):
+    """source https://gist.github.com/dobrosketchkun/f14e1ab9ae817b00b28251f11786fbcf"""
+    name = []
+    for _item in ref['author']:
+        given_in = _item['given'].split(' ')
+        given = ''.join([_name[0] + '.' for _name in given_in])
+        name.append(_item['family'] + ', ' + given + ', ')
+    return ''.join(name[0:-1]) + 'and ' + name[-1][0:-2]
+
+
+def main():
+    nargs = len(argv)
+    if nargs == 2:
+        orcid = argv[1]
+        prefix = '<li>'
+        suffix = '</li>'
+    elif nargs == 4:
+        orcid = argv[1]
+        prefix = argv[2]
+        suffix = argv[3]
+    else:
+        print(usage)
+        exit()
+
+    url = 'https://orcid.org/' + orcid
+    driver = webdriver.Firefox()
+    driver.get(url)
+    page_source = driver.page_source
+    driver.close()
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    doi_list = []
+    doi_re = re.compile('doi.org')
+    for doi in soup.find_all(string='DOI:'):
+        a = doi.parent.parent.findChild('a', href = doi_re)
+        href = a.get('href')
+        doi_list.append(href)
+
+    session = CrossRefClient()
+    for doi in doi_list:
+        out = session.doi2json(doi)
+        authors = names(out)
+        year = str(out['created']['date-parts'][0][0])
+        title = out['title']
+        journal_short = out['container-title-short']
+        volume = str(out['volume'])
+        if 'article-number' in out.keys():
+            pages = str(out['article-number'])
+        else:
+            pages = out['page']
+        print(prefix +
+              authors + ' ' +
+              year + '. ' +
+              title + '. ' +
+              journal_short + '. ' +
+              volume + ':' +
+              pages + '. ' +
+              'doi:' + doi + '.' +
+              suffix)
+
+
+if __name__ == '__main__':
+    main()
